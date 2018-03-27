6bb187a7ca167205be562ee526a78531f2fc8727
Bojan Baltić <bojan.baltic@gmail.com>
2018-03-28 00:43:55 +0200

    Determine all public keys

--- pem/index.js
+++ pem/index.js
@@ -7,17 +7,20 @@ request({
     json: true
 }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
+        pems = {};
         var keys = body['keys'];
         for(var i = 0; i < keys.length; i++) {
             //Convert each key to PEM
+            var key_id = keys[i].kid;
             var modulus = keys[i].n;
 			var exponent = keys[i].e;
             var key_type = keys[i].kty;
             var jwk = { kty: key_type, n: modulus, e: exponent};
             var pem = jwkToPem(jwk);
+            pems[key_id] = pem;
         }
-        console.log(JSON.stringify(pem));
+        console.log(pems);
     } else {
-        context.error("error");
+        console.error("error");
     }
 });
